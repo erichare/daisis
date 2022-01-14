73d274b8e41e2b73d6535c1f44a070177fa94af9
Eric Hare <ericrhare@gmail.com>
2022-01-13 20:03:42 -0700

    Create main.py

---
+++ titanic_statistics/main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+#load and process data into a global structure
+titanic = pd.read_csv("https://raw.githubusercontent.com/pandas-dev/pandas/master/doc/data/titanic.csv")
+
+def mean(field="Age"):
+    return float(titanic[field].mean())
+
+def median(field="Age"):
+    return float(titanic[field].median())
+
+def percentile(field="Age", percentile=.25):
+    return float(titanic[field].quantile(float(percentile)))
