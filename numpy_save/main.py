b8cf82bf14a96debe17eedecea39a0cacd3dfd90
Eric Hare <ericrhare@gmail.com>
2022-01-27 11:43:58 -0700

    Update main.py

--- numpy_save/main.py
+++ numpy_save/main.py
@@ -5,5 +5,6 @@ def compute():
     home = os.getenv("HOME")
     b = np.array([1, 1])
     np.save(home + '/b.npy', b)
-    return "Success"
+       c = np.load(home + '/b.npy')
+    return str(c[0])
   
