01ee808e171ab8d6dca09ccfc0848e6c62703bd0
Eric Hare <ericrhare@gmail.com>
2022-02-01 14:57:41 -0700

    Update main.py

--- io_prime/main.py
+++ io_prime/main.py
@@ -55,7 +55,7 @@ def io_bound(num_sentences: int=100000):
     if num_sentences > 1000000:
         raise AssertionError("Please reduce the num_sentences to < 1,000,000")
 
-    sentences = [lorem.sentence() for _ in range(num_sentences)]
+    sentences = [lorem.sentence() for _ in range(int(num_sentences))]
 
     with tempfile.TemporaryDirectory() as td:
         f_name = os.path.join(td, 'test')
