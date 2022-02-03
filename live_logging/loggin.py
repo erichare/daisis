7394f3ef8875ee56d0563628650b6acc619b3938
Eric Hare <eric@omnianalytics.io>
2022-02-03 13:31:32 -0700

    Update loggin.py

--- live_logging/loggin.py
+++ live_logging/loggin.py
@@ -1,7 +1,7 @@
 import time
 
 def live_log_test(firstNumber: float=4, secondNumber: float=5, delay: int=100):
-    for i in range(delay):
+    for i in range(int(delay)):
         time.sleep(1)
         print(i)
 
