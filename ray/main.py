4775b050cc1acbc4ec377582b47e71de56524287
Eric Hare <eric@omnianalytics.io>
2022-02-10 13:35:10 -0700

    Update main.py

--- ray/main.py
+++ ray/main.py
@@ -1,23 +1,23 @@
 import ray
 import time
 
+ray.init(local_mode=True)
+print(ray.nodes())
+
+@ray.remote
+def sleep(delay):
+    time.sleep(1)
+
 def compute(delay):
-    ray.init(local_mode=True)
     print(ray.nodes())
-    
-    @ray.remote
-    def sleep(delay):
-        time.sleep(1)
 
     print("Beginning Ray Compute...")
     time.sleep(1)
     start = time.time()
-    #print(ray.nodes())
     print("About to try")
     futures = [sleep.remote(float(delay)) for _ in range(5)]
     print("About to get...")
     ray.get(futures)
-    ray.shutdown()
 
     print(time.time() - start)
     return str(time.time() - start)
