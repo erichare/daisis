df05881d52b6e2fd470de59dddd144bb46240b7b
Eric Hare <eric@omnianalytics.io>
2022-02-10 12:13:05 -0700

    Update main.py

--- ray/main.py
+++ ray/main.py
@@ -1,30 +1,24 @@
 import ray
 import time
 
-ray.init()
-print(ray.nodes())
-
 @ray.remote
 def sleep(delay):
     time.sleep(1)
 
 def compute(delay):
+    ray.init()
+    print(ray.nodes())
+
     print("Beginning Ray Compute...")
     time.sleep(1)
     start = time.time()
     #print(ray.nodes())
     print("About to try")
-    try:
-        futures = [sleep.remote(float(delay)) for _ in range(5)]
-        print("About to get...")
-        ray.get(futures)
-    except Exception as e:
-        print("We failed")
-        print(e)
-        ray.shutdown()
-        ray.init()
-        futures = [sleep.remote(float(delay)) for _ in range(5)]
-        ray.get(futures)
+    futures = [sleep.remote(float(delay)) for _ in range(5)]
+    print("About to get...")
+    ray.get(futures)
+    ray.shutdown()
+
     print(time.time() - start)
     return str(time.time() - start)
 
