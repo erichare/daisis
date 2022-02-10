b03767a337a3362100a5971cd10d9d863f3bf26a
Eric Hare <eric@omnianalytics.io>
2022-02-10 10:30:31 -0700

    Create main.py

---
+++ ray/main.py
@@ -0,0 +1,18 @@
+import ray
+import time
+
+ray.init()
+
+@ray.remote
+def sleep(delay):
+    time.sleep(delay)
+
+def compute(delay):
+    start = time.time()
+    futures = [sleep.remote(float(delay)) for _ in range(15)]
+    ray.get(futures)
+    print(time.time() - start)
+    return str(time.time() - start)
+
+if __name__ == "__main__":
+    out = compute(1)
