8c91e417287c71b694a69bb608db49f062417933
Eric Hare <eric@omnianalytics.io>
2022-03-31 13:15:22 -0700

    Update main.py

--- start_streamlit/main.py
+++ start_streamlit/main.py
@@ -5,11 +5,13 @@ from pydaisi import Daisi
 def start(daisi_name, base_url="https://app.daisi.io"):
     d = Daisi(daisi_name, base_url=base_url)
 
+    r = requests.get("http://pebble-api/pebbles/streamlit")
+    result2 = r.json()
+
     my_id = str(d.id)
     r = requests.post("http://pebble-api/pebbles/streamlit", json={"id": my_id})
     result = r.json()
 
-    r = requests.get("http://pebble-api/pebbles/streamlit")
-    result2 = r.json()
+    mystr = f'Previous Streamlit App: {result_2}<br>New Streamlit App: {result}<br><br>View it here: <a target="_blank" href="{base_url}/st>Click!</a>"'
 
-    return [result, result2]
+    return mystr
