90dbf49ed0570e15a1c80e09750b64b84c04d760
Eric Hare <eric@omnianalytics.io>
2022-03-31 12:41:52 -0700

    Update main.py

--- start_streamlit/main.py
+++ start_streamlit/main.py
@@ -2,8 +2,8 @@ import requests
 
 from pydaisi import Daisi
 
-def start(daisi_name):
-    d = Daisi(daisi_name)
+def start(daisi_name, base_url="https://app.daisi.io"):
+    d = Daisi(daisi_name, base_url=base_url)
 
     my_id = str(d.id)
     r = requests.post("http://pebble-api/pebbles/streamlit", json={"id": my_id})
