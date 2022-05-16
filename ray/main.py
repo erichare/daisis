74e5ae24a689385f0a40fc82f800bb82a8c428cb
Eric Hare <eric@omnianalytics.io>
2022-05-16 13:34:14 -0700

    Update main.py

--- ray/main.py
+++ ray/main.py
@@ -2,7 +2,7 @@ import ray
 import time
 import numpy as np
 
-ray.init(address="auto")
+ray.init(address='pebble-api-worker-748bb67786-j8p7q:6380')
 
 def compute(val="hi"):
     return val + " bye"
