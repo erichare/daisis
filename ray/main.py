0ebd328634c6c1d70d08d8cfe85db2a477d31d9d
Eric Hare <eric@omnianalytics.io>
2022-05-16 15:07:06 -0700

    Update main.py

--- ray/main.py
+++ ray/main.py
@@ -5,14 +5,14 @@ import numpy as np
 ray.init(address='localhost:6380')
 
 @ray.remote
-def sleep_function(name = "VM1 ", duration = 10):
+def sleep_function(name: str = "VM1 ", duration: int = 10):
     for i in range(duration):
         time.sleep(1)
         print(name, str(np.random.rand()))
     
     return 1
 
-def compute(duration = 10, nb_procs=4, name = "VM1"):
+def compute(duration: int = 10, nb_procs: int = 4, name: str = "VM1"):
     start = time.time()
     futures = [sleep_function.remote(name = name, duration = duration) for _ in range(nb_procs)]
     ray.get(futures)
