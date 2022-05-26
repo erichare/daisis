c9ad12cada83c53ae4cd7b616dd237224457f05a
Eric Hare <eric@omnianalytics.io>
2022-05-26 10:51:23 -0700

    Update main.py

--- hello_streamlit/main.py
+++ hello_streamlit/main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('Uber pickups in NYC!')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
