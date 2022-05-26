55639391b24820df00182ed33053c3e466289d2b
Eric Hare <ericrhare@gmail.com>
2022-05-26 15:42:55 -0700

    Create main.py

---
+++ add_two_numbers_error/main.py
@@ -0,0 +1,7 @@
+raise AssertionError("Failed to load Daisi.")
+
+def compute(firstNumber: float=4, secondNumber: float=5):
+    # Compute the add
+    result = firstNumber + secondNumber
+
+    return result
