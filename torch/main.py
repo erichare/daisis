c4fb1898f2b8a3808016279aece71e935e4ca524
Eric Hare <ericrhare@gmail.com>
2022-07-05 13:51:40 -0700

    Update main.py

--- torch/main.py
+++ torch/main.py
@@ -1,7 +1,7 @@
 import torch
 
 def compute(firstNumber: float=4, secondNumber: float=5):
-    x = torch.rand(firstNumber, secondNumber)
+    x = torch.rand(int(firstNumber), int(secondNumber))
     print(x)
 
     return x
