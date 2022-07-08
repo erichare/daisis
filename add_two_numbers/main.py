c97b8a62dbafa01ea2c8dadd393836fa6caa1f76
Eric Hare <ericrhare@gmail.com>
2022-07-08 15:41:07 -0700

    Update main.py

--- add_two_numbers/main.py
+++ add_two_numbers/main.py
@@ -1,5 +1,11 @@
-def compute(firstNumber: float=4, secondNumber: float=5):
+def add(firstNumber: float=4, secondNumber: float=5):
     # Compute the add
     result = firstNumber + secondNumber
+
+    return result
+
+def subtract(firstNumber: float=4, secondNumber: float=5):
+    # Compute the add
+    result = firstNumber - secondNumber
 
     return result
