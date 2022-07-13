c43efeef025aa2d17ec73725fabc89f38ad208c5
Eric Hare <eric@omnianalytics.io>
2022-07-13 09:53:54 -0700

    Update main.py

--- add_two_numbers/main.py
+++ add_two_numbers/main.py
@@ -1,6 +1,10 @@
+import time
+
 def add(firstNumber: float=4, secondNumber: float=5):
     # Compute the add
+    time.sleep(1)
     print("Hi")
+    time.sleep(1)
     result = firstNumber + secondNumber
 
     return result
