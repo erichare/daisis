36bd304dfd5e7a2b82c6ddf68bb6ee6287e2088a
Eric Hare <ericrhare@gmail.com>
2022-08-11 08:12:00 -0700

    Tensorflow bug test

---
+++ add_two_numbers_tensor/main.py
@@ -0,0 +1,8 @@
+import tensorflow
+import pandas as pd
+
+def compute(firstNumber: float=4, secondNumber: float=5):
+    # Compute the add
+    result = firstNumber + secondNumber
+
+    return result
