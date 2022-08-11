7b92a80f0db8c9c4f8107f582e1accce072b93d7
Eric Hare <ericrhare@gmail.com>
2022-08-11 14:19:07 -0700

    Create main.py

---
+++ add_two_numbers_dlib/main.py
@@ -0,0 +1,13 @@
+import dlib
+
+def add(firstNumber: float=4, secondNumber: float=5):
+    # Compute the add
+    result = firstNumber + secondNumber
+
+    return result
+
+def subtract(firstNumber: float=4, secondNumber: float=5):
+    # Compute the add
+    result = firstNumber - secondNumber
+
+    return result
