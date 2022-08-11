d40d94b2a48933b7924d9ce1700d901fed5b3662
Eric Hare <ericrhare@gmail.com>
2022-08-11 08:16:09 -0700

    Add streamlit

--- add_two_numbers_tensor/main.py
+++ add_two_numbers_tensor/main.py
@@ -1,8 +1,12 @@
 import tensorflow
 import pandas as pd
+import streamlit as st
 
 def compute(firstNumber: float=4, secondNumber: float=5):
     # Compute the add
     result = firstNumber + secondNumber
 
     return result
+
+st.title('Add Two Numbers!')
+st.text(compute(5, 6))
