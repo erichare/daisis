a2c0cb786337a2e069a9085d1812d3ec19766a02
Eric Hare <ericrhare@gmail.com>
2022-08-31 08:23:09 -0700

    Update main.py

--- add_two_numbers/main.py
+++ add_two_numbers/main.py
@@ -2,11 +2,14 @@ import time
 import numpy as np
 import os
 
+test = print(os.environ)
+
 def add_test(firstNumber: float=4, secondNumber: float=5):
     # Compute the add
     print(os.environ)
     time.sleep(1)
     print("Hi")
+    print(test)
     time.sleep(1)
     result = firstNumber + secondNumber
 
