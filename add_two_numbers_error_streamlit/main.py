981a1745a169455ce23a5c37cd3463f68c25a072
Eric Hare <ericrhare@gmail.com>
2022-09-07 16:30:33 -0700

    Update main.py

--- add_two_numbers_error_streamlit/main.py
+++ add_two_numbers_error_streamlit/main.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import os
 
-print(os.environ)
+x / 5
 
 def compute(firstNumber: float=4, secondNumber: float=5):
     # Compute the add
     result = firstNumber + secondNumber
     print(os.environ)
 
-    return os.environ
+    return result
 
 st.title('Add Two Numbers!')
 st.text(compute(5, 6))
