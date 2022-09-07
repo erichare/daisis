a7a0628829800d3f0ee85ed0b66763790e3437ac
Eric Hare <ericrhare@gmail.com>
2022-09-07 12:57:23 -0700

    Create main.py

---
+++ tensorflow/main.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+print(tf.test.is_gpu_available())
+print(tf.test.is_built_with_cuda())
+
+def add_test(firstNumber: float=4, secondNumber: float=5):
+    result = firstNumber + secondNumber
+
+    print(tf.test.is_gpu_available())
+    print(tf.test.is_built_with_cuda())
+
+    return result
+
+def subtract(firstNumber: float=4, secondNumber: float=5):
+    # Compute the add
+    result = firstNumber - secondNumber
+
+    return result
