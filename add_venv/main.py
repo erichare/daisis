f7c5685ebe85c4a1183fb5215787d250852865b8
Eric Hare <ericrhare@gmail.com>
2022-09-08 07:53:46 -0700

    Create main.py

---
+++ add_venv/main.py
@@ -0,0 +1,25 @@
+import time
+import numpy as np
+import os # Test
+import potatoxgfdgdfgdf
+
+test = os.environ
+
+print("I'm testing a load print!")
+
+def add_test(firstNumber: float=4, secondNumber: float=5):
+    # Compute the add
+    print(os.environ)
+    time.sleep(1)
+    print("Hi")
+    print(test)
+    time.sleep(1)
+    result = firstNumber + secondNumber
+
+    return result
+
+def subtract(firstNumber: float=4, secondNumber: float=5):
+    # Compute the add
+    result = firstNumber - secondNumber
+
+    return result
