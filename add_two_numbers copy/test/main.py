6f4c612929e39ba39c773ccf55427d0f5c43c3b1
Eric Hare <ericrhare@gmail.com>
2022-10-12 11:59:35 -0700

    TEST nesting

---
+++ add_two_numbers copy/test/main.py
@@ -0,0 +1,24 @@
+import time
+import numpy as np
+import os # Test
+
+test = os.environ
+
+print("I'm testing a load print!")
+
+def add_test(firstNumber: float=4, secondNumber: float=5):
+    # Compute the add
+    print(os.environ)
+    time.sleep(1)
+    print("Hi")
+    print(test)
+    time.sleep(1)
+    result = firstNumber + secondNumber
+
+    return result
+
+def subtract(firstNumber: float=4, secondNumber: float=5):
+    # Compute the add
+    result = firstNumber - secondNumber
+
+    return result
